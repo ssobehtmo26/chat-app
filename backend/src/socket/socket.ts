2367255fa2830c932fcc6e2dd06335a8f9dc092e
ssobehtmo26 <writetoom2003@gmail.com>
2024-08-26 10:16:08 +0530

    first

---
+++ backend/src/socket/socket.ts
@@ -0,0 +1,35 @@
+import { Server } from "socket.io";
+import http from "http";
+import express from "express";
+
+const app = express();
+
+const server = http.createServer(app);
+const io = new Server(server, {
+	cors: {
+		origin: ["http://localhost:5173"],
+		methods: ["GET", "POST"],
+	},
+});
+
+export const getReceiverSocketId = (receiverId: string) => {
+	return userSocketMap[receiverId];
+};
+
+const userSocketMap: { [key: string]: string } = {}; 
+
+io.on("connection", (socket) => {
+
+	const userId = socket.handshake.query.userId as string;
+
+	if (userId) userSocketMap[userId] = socket.id;
+
+	io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+	socket.on("disconnect", () => {
+		delete userSocketMap[userId];
+		io.emit("getOnlineUsers", Object.keys(userSocketMap));
+	});
+});
+
+export { app, io, server };
